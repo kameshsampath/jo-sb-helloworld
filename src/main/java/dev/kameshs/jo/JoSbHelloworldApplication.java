c316e6ac8af0609229ead1563c3ef50fa783d136
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-06-06 13:59:37 +0530

    Repo Init

---
+++ src/main/java/dev/kameshs/jo/JoSbHelloworldApplication.java
@@ -0,0 +1,13 @@
+package dev.kameshs.jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoSbHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoSbHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kameshs/jo/JoSbHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kameshs.jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoSbHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
